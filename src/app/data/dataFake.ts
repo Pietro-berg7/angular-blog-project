906b22f362b09fa12e62f63aeb0e439e02bcdc5d
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-19 14:11:08 -0300

    Deixando a content Page Dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,33 +1,33 @@
 export const dataFake = [
   {
-    id: 1,
-    photo:
-      'https://ovicio.com.br/wp-content/uploads/2023/06/20230614-ovicio-rick-morty-season-7-730x365.jpg',
+    id: '1',
     title: 'Rick and Morty: humor ácido e viagens cósmicas.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
+    photoCover:
+      'https://ovicio.com.br/wp-content/uploads/2023/06/20230614-ovicio-rick-morty-season-7-730x365.jpg',
   },
   {
-    id: 2,
-    photo: 'https://dark.netflix.io/share/global.png',
+    id: '2',
     title: 'Dark: onde passado, presente e futuro colidem.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
+    photoCover: 'https://dark.netflix.io/share/global.png',
   },
   {
-    id: 3,
-    photo:
-      'https://cinepop.com.br/wp-content/uploads/2020/08/Supernatural-1.jpg',
+    id: '3',
     title: 'Supernatural: uma saga de caçadores destemidos.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
+    photoCover:
+      'https://cinepop.com.br/wp-content/uploads/2020/08/Supernatural-1.jpg',
   },
   {
-    id: 4,
-    photo:
-      'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2018/01/walter-white-breaking-bad-grande.jpg',
+    id: '4',
     title: 'Breaking Bad: a descida ao submundo do crime.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
+    photoCover:
+      'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2018/01/walter-white-breaking-bad-grande.jpg',
   },
 ];
