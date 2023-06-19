374b685202cc02999a8661abf3e642b4470d65e8
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-19 13:42:52 -0300

    Pegando os dados dinamicamente

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,4 +7,27 @@ export const dataFake = [
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
   },
+  {
+    id: 2,
+    photo: 'https://dark.netflix.io/share/global.png',
+    title: 'Dark: onde passado, presente e futuro colidem.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
+  },
+  {
+    id: 3,
+    photo:
+      'https://cinepop.com.br/wp-content/uploads/2020/08/Supernatural-1.jpg',
+    title: 'Supernatural: uma saga de caçadores destemidos.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
+  },
+  {
+    id: 4,
+    photo:
+      'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2018/01/walter-white-breaking-bad-grande.jpg',
+    title: 'Breaking Bad: a descida ao submundo do crime.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni provident qui ad debitis numquam, excepturi voluptas esse tempore, cumque explicabo nam saepe fuga dolorem delectus molestias blanditiis, ea laborum?',
+  },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -10,7 +11,9 @@ export class ContentComponent implements OnInit {
   contentTitle: string = '';
   contentDescription: string = '';
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => console.log(value.get('id')));
+  }
 }
